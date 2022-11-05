8ef089e90a09c93b7954db9b343c2cbc76dce3b1
batuhansicakyuz <batuhansicakyuz@gmail.com>
2022-11-05 09:59:24 +0300

    fourth commit

---
+++ src/easy/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        int[] numbs = new int[]{ 1,2,3,4, };
+        System.out.println(cd.containsDuplicate(numbs));
+    }
+    public boolean containsDuplicate(int[] numbs){
+        HashSet<Integer> numbers = new HashSet<>();
+
+        for (int i=0; i<numbs.length; i++) {
+            if(numbers.contains(numbs[i])) return true;
+            numbers.add(numbs[i]);
+        }
+        return false;
+    }
+}
