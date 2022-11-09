8c5e967c6ccf22d1c40f920b2e56f07033fffd92
batuhansicakyuz <batuhansicakyuz@gmail.com>
2022-11-09 21:22:25 +0300

    sixth commit

---
+++ src/easy/ValidParentheses.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+
+        ValidParentheses vp = new ValidParentheses();
+        String s = "{[]";
+        System.out.println(vp.isValid(s));
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+
+        }
+        return stack.isEmpty();
+    }
+
+}
